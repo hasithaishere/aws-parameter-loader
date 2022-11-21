47d437aee5506d21f485e461a546ac5afd568ff3
Hasitha Gamage <hasitha@rocketbots.io>
2022-11-21 18:05:15 +0800

    Enable caching

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const SSM = require('./repository/ssm');
 
-module.exports = async (options = {}) => {
+module.exports = async (options = { CACHE_ENABLED: true }) => {
+    if (CACHE_ENABLED && process.env.PARAM_CACHED) return;
+
     const ssmOptions = {
-        parameterBasePath: opt.parameterBasePath || process.env.PARAM_PATH,
-        region: opt.region || process.env.AWS_REGION
+        parameterBasePath: options.parameterBasePath || process.env.PARAM_PATH,
+        region: options.region || process.env.AWS_REGION
     };
 
     const ssm = new SSM(ssmOptions);
     const result = await ssm.loadParameters();
     SSM.loadParametersToEnv(result, ssmOptions.parameterBasePath);
-    return result;
+    process.env.PARAM_CACHED = true;
 };
