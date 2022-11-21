dd761556106e2e4fd63955b946e0d62eec655cf7
Hasitha Gamage <hasitha@rocketbots.io>
2022-11-21 16:38:16 +0800

    Fix options duplicate issue

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const SSM = require('./repository/ssm');
 
 module.exports = async (options = {}) => {
-    const options = {
+    const ssmOptions = {
         parameterBasePath: opt.parameterBasePath || process.env.PARAM_PATH,
         region: opt.region || process.env.AWS_REGION
     };
 
-    const ssm = new SSM(options);
+    const ssm = new SSM(ssmOptions);
     const result = await ssm.loadParameters();
-    SSM.loadParametersToEnv(result, options.parameterBasePath);
+    SSM.loadParametersToEnv(result, ssmOptions.parameterBasePath);
     return result;
 };
